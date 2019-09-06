505556c160b9b1b20f2edc39f62c2623a90ce681
taha shajarian <33688478+tahashajarian@users.noreply.github.com>
2019-09-07 03:53:08 +0430

    add serviceWorkers to work in offline mode
    service workers not work in development mode

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.scss';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
@@ -11,4 +12,5 @@ ReactDOM.render(
     </Provider>
 , document.getElementById('root'));
 
+serviceWorker.register();
 
